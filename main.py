b6594c96c6509aeebae6351e7beb20592ef32e4b
MaGo1024 <olumideodusote@gmail.com>
2026-02-01 11:14:44 +0000

    Generate a random walk

--- main.py
+++ main.py
@@ -1,58 +1,18 @@
-from turtle import Turtle as t, Screen
+import turtle as t
 import random
 
-import polygon
 
-tim = t()
+tim = t.Turtle()
 
+tim.pensize(10)
+tim.speed("fastest")
+colours = ["CornflowerBlue", "SpringGreen", "RosyBrown", "wheat", "RoyalBlue", "OliveDrab", "firebrick", "SlateGray", "DarkOrchid"]
+directions = [0, 90, 180, 270]
 
-tim.shape("turtle")
-tim.color("red")
-tim.color("LightSalmon4")
+for _ in range(200):
+    tim.color(random.choice(colours))
+    tim.forward(30)
+    tim.setheading(random.choice(directions))
 
-# Draw a square
-# for i in range(4):
-#     tim.forward(100)
-#     tim.right(90)
-
-# Draw a dashed line
-# for _ in range(15):
-#     tim.forward(10)
-#     tim.penup()
-#     tim.forward(10)
-#     tim.pendown()
-
-# Drawing different shapes
-# for _ in range(3):
-#     tim.forward(100)
-#     tim.right(120)
-#
-# for i in range(4):
-#     tim.forward(100)
-#     tim.right(90)
-#
-# for i in range(5):
-#     tim.forward(100)
-#     tim.right(72)
-colors = ["red", "green", "purple", "yellow", "pink","LightSalmon4", "SlateGrey","SeaGreen","CornflowerBlue"]
-
-def draw_shape(num_of_sides):
-    angle = 360 / num_of_sides
-    for _ in range(num_of_sides):
-        tim.forward(100)
-        tim.right(angle)
-
-# shapes = [3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
-
-for shape_side_n in range(3, 11):
-    tim.color(random.choice(colors))
-    draw_shape(shape_side_n)
-
-
-screen = Screen()
-screen.exitonclick()
-
-# import heroes
-# print(heroes.gen())
 
 
