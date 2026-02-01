835c53f41671c566a89634df2b4dd362a42a8922
MaGo1024 <olumideodusote@gmail.com>
2026-02-01 17:10:50 +0000

    Python tuples and generating random colors

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 import turtle as t
 import random
 
-
+t.colormode(255)
 tim = t.Turtle()
+
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    color = (r, g, b)
+    return color
 
 tim.pensize(10)
 tim.speed("fastest")
-colours = ["CornflowerBlue", "SpringGreen", "RosyBrown", "wheat", "RoyalBlue", "OliveDrab", "firebrick", "SlateGray", "DarkOrchid"]
+# colours = ["CornflowerBlue", "SpringGreen", "RosyBrown", "wheat", "RoyalBlue", "OliveDrab", "firebrick", "SlateGray", "DarkOrchid"]
 directions = [0, 90, 180, 270]
 
 for _ in range(200):
-    tim.color(random.choice(colours))
+    tim.color(random_color())
     tim.forward(30)
     tim.setheading(random.choice(directions))
 
