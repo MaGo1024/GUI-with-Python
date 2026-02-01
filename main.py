9e9ab5dd3f0e48baa8b540c6e865ba16ef824653
MaGo1024 <olumideodusote@gmail.com>
2026-02-01 17:58:32 +0000

    Draw Spirograph - 360 degrees

--- main.py
+++ main.py
@@ -12,10 +12,13 @@ def random_color():
     color = (r, g, b)
     return color
 
-for _ in range(100):
-    tim.color(random_color())
-    tim.circle(100)
-    tim.setheading(tim.heading() + 10)
+def draw_spirograph(size_of_gap):
+    for _ in range(int(360/size_of_gap)):
+        tim.color(random_color())
+        tim.circle(100)
+        tim.setheading(tim.heading() + size_of_gap)
+
+draw_spirograph(size_of_gap=5)
 
 screen = t.Screen()
 screen.exitonclick()
